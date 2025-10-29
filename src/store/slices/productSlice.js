2089cbf0d52863d9b2441484790f33636ba9242e
magzumAlmat <almat.magzum@gmail.com>
2025-10-29 22:19:50 +0500

    ha5dsaddsdddddddadadsdd3ddasdddвфывввdasdddddD

--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -15,7 +15,7 @@ let initialState = {
   selectedMainType: "Все товары",
   selectedType: "",
   clickCount: 0,
-  host: 'api/api/store',
+  host: 'api/store',
   filters: {
     searchTerm: "",
     sortBy: "",
