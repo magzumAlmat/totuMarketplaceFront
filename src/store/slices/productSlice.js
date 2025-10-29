018814573ce975542fb24a98d3baa2d84a39e391
magzumAlmat <almat.magzum@gmail.com>
2025-10-29 15:42:42 +0500

    pre deplpoy 17

--- src/store/slices/productSlice.js
+++ src/store/slices/productSlice.js
@@ -15,7 +15,7 @@ let initialState = {
   selectedMainType: "Все товары",
   selectedType: "",
   clickCount: 0,
-  host: '/api/store/',
+  host: 'https://totu.kz/api/store/',
   filters: {
     searchTerm: "",
     sortBy: "",
